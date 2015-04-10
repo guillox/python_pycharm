cb1f47ec00a2a09b4204845535fd9d3cdaa9e780
jose guillermo <guillox22@gmail.com>
2015-04-10 19:39:38 -0300

    tp2ej07  v0.1

---
+++ tp2ej7.py
@@ -0,0 +1,24 @@
+# coding=utf-8
+__author__ = 'guillermo'
+"""7. Realice un script que imprima la suma de los primeros 10 números impares.
+
+
+
+"""
+#Con while
+cant = 0
+suma = 0
+num = 1
+while cant < 10:
+    if num % 2 != 0:
+        cant += 1
+        suma += num
+        print num
+    num += 1
+print 'la suma de los primero numero impares es:'suma
+
+
+
+
+
+
