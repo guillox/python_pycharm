6882342e0af5e0becc8267fa81ea31359e1dcb26
jose guillermo <guillox22@gmail.com>
2015-04-10 19:45:55 -0300

    tp2ej04  v0.1

---
+++ tp2ej4.py
@@ -0,0 +1,12 @@
+# coding=utf-8
+__author__ = 'guillermo'
+"""4. Construya una estructura de datos para almacenar información acerca de eventos,guardando para cada evento una
+descripción, fecha y hora,lista de invitados y si es público o no. Agregue algunos eventos como ejemplo, imprima la
+cantidad de invitados de alguno en particular, agréguele dos invitados y elimine uno de otro evento.
+¿Cómo haría si quisiera agregarle un estado a los invitados que indique si el mismo confirmó asistencia, aún no la
+confirmó o bien la rechazó? Modifique la estructura para ello"""
+
+informacion = []
+descripcion = raw_input('ingrese la descripcion del evento')
+
+
