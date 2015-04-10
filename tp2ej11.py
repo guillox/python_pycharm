db1ef5b2a785c65b77f42bbfe36fab3c28878ea1
jose guillermo <guillox22@gmail.com>
2015-04-10 16:53:32 -0300

    Initial commit

---
+++ tp2ej11.py
@@ -0,0 +1,14 @@
+# coding=utf-8
+__author__ = 'guillermo'
+"""11. Realice un script que guarde en una lista los primeros N números primos, donde N se
+ingresa por teclado.
+
+Los números primos son aquellos números enteros que sólo son divisibles por si mismos y por la unidad".
+Ejemplos: 1,2,3,5,7,11,13,17,19..., -1,-2,-3,...
+"""
+
+lnump = []
+N = raw_input('Ingrese la cantidad de numeros primos: ')
+
+for i in range(0, N+1):
+
